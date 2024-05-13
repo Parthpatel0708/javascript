8831afdf31f1ede8990877432cf377a6214458ef
Parthpatel0708 <121258121+Parthpatel0708@users.noreply.github.com>
2024-05-13 21:23:48 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=1234
+let  Fname ="parth patel";
+var accountPassword = 521;
+
+/*prefer not to use var
+because of issue in block scope and functional scope  */
+
+//scope = Scope is defined in for loops by {} this to show the variable is define for specific scope// 
+
+
+eamil="parthpatel@gmail.com"
+// not good way to declear the variable//
+
+console.log(accountId)
+
+console.table({accountId,Fname,accountPassword,eamil})
+// { } this are important //
